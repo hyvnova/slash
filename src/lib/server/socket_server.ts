afed34d55b1f0f0aaecc8716be79001121e81733
Hendrick <70229868+EZSNoVa@users.noreply.github.com>
2023-12-04 12:49:17 +0000

    stash

--- src/lib/server/socket_server.ts
+++ src/lib/server/socket_server.ts
@@ -1,10 +1,10 @@
-import { Server, type ServerOptions } from 'socket.io';
+import { Server, Socket, type ServerOptions } from 'socket.io';
 import { connect, disconnect, get_username, is_online } from './db/socket';
 import { Events, type MessageType } from './../types';
 
 
 // @ts-ignore
-function get_chat_id(socket): string {
+function get_chat_id(socket: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>): string {
     return Array.from(socket.rooms)[1] as string;
 }
 
@@ -18,13 +18,13 @@ export default function injectSocketIO(server: Partial<ServerOptions> | undefine
         ...server,
     });
 
-
     io.on('connection', (socket) => {
-
-        socket.on('user connect', async (username: string) => {
+        socket.on(Events.connect, (username: string) => {
             socket.join(username);
-            await connect(socket.id, username);
-            console.debug('connected', username);
+
+            connect(socket.id, username);
+
+            console.debug(username, 'connected');
         });
 
         socket.on('disconnect', async () => {
@@ -32,14 +32,30 @@ export default function injectSocketIO(server: Partial<ServerOptions> | undefine
             await disconnect(socket.id);
         });
 
+        /** 
+         * Join Chat
+         */
+        socket.on(Events.join_chat, async (chat_id: string) => {
+            console.debug("join_chat", chat_id)
+            console.debug(await get_username(socket.id), 'joined chat');
+
+            // Leave all other rooms
+            Object.keys(socket.rooms).forEach((room) => {
+                if (room !== socket.id) {
+                    socket.leave(room);
+                }
+            });
+            socket.join(chat_id);
+        });
+
         /**
          * Set Status
          */
-        socket.on('set status', async (status: string, friends: string[]) => {
+        socket.on(Events.set_status, async (status: string, friends: string[]) => {
             let username = await get_username(socket.id);
             friends.forEach(async (friend) => {
                 if (await is_online(friend)) {
-                    io.to(friend).emit('status', status, username);
+                    io.to(friend).emit(Events.status, status, username);
                 }
             });
         });
@@ -48,24 +64,43 @@ export default function injectSocketIO(server: Partial<ServerOptions> | undefine
          * Friend Requests 
          * Used to update other's friend requests UI on the fly
          */
-        const friend_requests_states = ['new', 'cancel', 'accept', 'reject'];
+        const friend_requests_states = [
+            Events.new_friend_request,
+            Events.cancel_friend_request,
+            Events.accept_friend_request,
+            Events.reject_friend_request,
+        ]
         for (const state of friend_requests_states) {
-            socket.on(`${state} friend request`, async (friend: string) => {
+            socket.on(state, async (friend: string) => {
 
                 let username = await get_username(socket.id);
-                console.debug(`${state} friend request`, friend, username);
+                console.debug(state, friend, username);
 
                 if (await is_online(friend)) {
-                    io.to(friend).emit(`${state} friend request`, username);
+                    io.to(friend).emit(state, username);
                 }
             });
         }
 
-        socket.on('unfriend', async (friend: string) => {
+        socket.on(Events.unfriend, async (friend: string) => {
             if (await is_online(friend)) {
-                io.to(friend).emit('unfriend', await get_username(socket.id));
+                io.to(friend).emit(Events.unfriend, await get_username(socket.id));
             }
         });
 
+        /**
+         * Messages: send, delete, edit
+         */
+        socket.on(Events.new_message, async (message: Partial<MessageType>) => {
+            io.to(get_chat_id(socket)).emit(Events.new_message, message);
+        });
+
+        socket.on(Events.delete_message, async (message_id: string) => {
+            io.to(get_chat_id(socket)).emit(Events.delete_message, message_id);
+        });
+
+        socket.on(Events.edit_message, async (message: MessageType) => {
+            io.to(get_chat_id(socket)).emit(Events.edit_message, message);
+        });
     });
 }
