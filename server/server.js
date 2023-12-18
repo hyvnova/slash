a94f900b22ed2ff915e35410fd7c5b9a4ce423ee
Hendrick <70229868+EZSNoVa@users.noreply.github.com>
2023-12-18 15:16:01 +0000

    Before Svelte 4 migration

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 import http from 'http';
 import express from 'express';
-import { handler } from './../build/handler.js';
 import injectSocketIO from './socket_server.js';
 const app = express();
 const server = http.createServer(app);
@@ -8,7 +7,6 @@ const server = http.createServer(app);
 // @ts-ignore
 injectSocketIO(server);
 // SvelteKit handlers
-app.use(handler);
 server.listen(3000, () => {
     console.log('Running on http://localhost:3000');
 });
