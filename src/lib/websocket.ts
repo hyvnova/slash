1c95c0673692faa81d8a6c2a3993ef66dd20ee2c
EZSNoVa <thenovaggamer@gmail.com>
2023-12-22 18:54:10 -0500

    Production socket server

--- src/lib/websocket.ts
+++ src/lib/websocket.ts
@@ -1,3 +1,3 @@
 import client from 'socket.io-client';
 
-export const ws = process.env.NODE_ENV === "production" ? client() : client("http://localhost:3001") 
+export const ws = process.env.NODE_ENV === "production" ? client("https://slash-socket-server.ezsnova.repl.co/") : client("http://localhost:3001") 
