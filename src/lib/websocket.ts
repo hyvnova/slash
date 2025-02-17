9a28403e7d3c96997ff35c0dcb9447ac86f688aa
Hyvnt <hendrickrodriguez.py@gmail.com>
2025-02-17 17:03:48 -0500

    kill me

--- src/lib/websocket.ts
+++ src/lib/websocket.ts
@@ -4,10 +4,10 @@ import toast from './stores/toast';
 
 export let ws: Socket<DefaultEventsMap, DefaultEventsMap>;
 
-ws = process.env.NODE_ENV === "production" ?
-    client("https://server-autumn-water-8460.fly.dev/")
-    :
-    client("http://localhost:3003")
+ws =
+	process.env.NODE_ENV === 'production'
+		? client('https://slash-socket-server-production.up.railway.app/')
+		: client('http://localhost:3003');
 
 ws.on("connect_error", (err) => {
     if (process.env.NODE_ENV !== "production"){ console.error(err); }
